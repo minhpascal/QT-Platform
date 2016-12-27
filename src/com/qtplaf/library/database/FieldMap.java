c83d36faf7435fd241ac5418c6b4404bc90c55d7
Miquel Sas <miquelsas@gmail.com>
2016-12-27 18:55:23 +0100

    Added missing licence agreements.

--- src/com/qtplaf/library/database/FieldMap.java
+++ src/com/qtplaf/library/database/FieldMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Miquel Sas
+ * Copyright (C) 2015 Miquel Sas
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
